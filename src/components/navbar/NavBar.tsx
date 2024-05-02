cb02c52fd6955b46f3b62cb869998fc5c6a0778f
JuanLucca846 <lucca470@gmail.com>
2024-05-02 15:36:30 -0300

    Fix: Arrumando Tailwind Css

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../assets/NOSSO_1.png";
+import logo from "../../assets/logoEditado.png";
 
 function Navbar() {
   return (
     <>
       <div className="w-full bg-red-900 text-white flex justify-center py-4">
-        <div className="container flex justify-between text-lg">
-          <div className="text-2xl font-bold uppercase flex">
-            <img src={logo} className="h-20 w=20" />
-            NossoPomar
+        <div className="container flex justify-between text-lg items-center">
+          <div className="text-2xl font-bold uppercase flex items-center">
+            <img src={logo} className="h-20 w=20 " />
+            <h1>NossoPomar</h1>
           </div>
           <div className="flex gap-4">
             <Link to="/" className="hover:underline">
