351e88bc9063b85c249746f5f215532eb8910022
Estela Satie <estela.tatie@hotmail.com>
2024-05-07 15:28:51 -0300

    footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,16 +10,16 @@ function Footer() {
   return (
     
     <div className="flex justify-center bg-mint-cream text-battleship-gray">
-       {<img src={logo} className="h-20 w-20" />}
-      <div className="container flex flex-col items-center py-4">
+       {<img src={logo} className="h-auto w-20" />}
+      <div className="container flex flex-col items-center py-1.5">
         <p className="text-xl font-bold"> 
           NossoPomar | Copyright:{" NossoPomar"}
         </p>
         <p className="text-lg">Acesse nossas redes sociais</p>
         <div className="flex gap-2">
-          <LinkedinLogo size={30} weight="bold" />
-          <InstagramLogo size={30} weight="bold" />
-          <FacebookLogo size={30} weight="bold" />
+          <LinkedinLogo size={25} weight="bold" />
+          <InstagramLogo size={25} weight="bold" />
+          <FacebookLogo size={25} weight="bold" />
         </div>
       </div>
     </div>
