63db7c1700f1bace3eeafb2691a3f3564c9bd0d1
Estela Satie <estela.tatie@hotmail.com>
2024-05-07 14:05:52 -0300

    footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,3 +1,4 @@
+import logo from "../../assets/footer.png";
 import {
   FacebookLogo,
   InstagramLogo,
@@ -7,19 +8,22 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className=" flex justify-center bg-red-900 text-white">
+    
+    <div className="flex justify-center bg-mint-cream text-battleship-gray">
+       {<img src={logo} className="h-20 w-20" />}
       <div className="container flex flex-col items-center py-4">
-        <p className="text-xl font-bold">
+        <p className="text-xl font-bold"> 
           NossoPomar | Copyright:{" NossoPomar"}
         </p>
         <p className="text-lg">Acesse nossas redes sociais</p>
         <div className="flex gap-2">
-          <LinkedinLogo size={48} weight="bold" />
-          <InstagramLogo size={48} weight="bold" />
-          <FacebookLogo size={48} weight="bold" />
+          <LinkedinLogo size={30} weight="bold" />
+          <InstagramLogo size={30} weight="bold" />
+          <FacebookLogo size={30} weight="bold" />
         </div>
       </div>
     </div>
+    
   );
 }
 
