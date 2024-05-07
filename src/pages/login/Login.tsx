3d4bed9df222c7aee9dc0b0776ca978aababd2fc
JuanLucca846 <lucca470@gmail.com>
2024-05-07 14:43:36 -0300

    Fix: Cadastro e Login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,12 +1,45 @@
-import React, { ChangeEvent } from 'react'
-import { RotatingLines } from 'react-loader-spinner';
-import { Link } from 'react-router-dom';
+import React, { ChangeEvent, useContext, useEffect, useState } from "react";
+import { RotatingLines } from "react-loader-spinner";
+import { Link, useNavigate } from "react-router-dom";
+import UsuarioLogin from "../../models/UsuarioLogin";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Login() {
+  let navigate = useNavigate();
+
+  const [usuarioLogin, setUsuarioLogin] = useState<UsuarioLogin>(
+    {} as UsuarioLogin
+  );
+
+  const { usuario, handleLogin } = useContext(AuthContext);
+
+  const { isLoading } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (usuario.token !== "") {
+      navigate("/home");
+    }
+  }, [usuario]);
+
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+    setUsuarioLogin({
+      ...usuarioLogin,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function login(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+    handleLogin(usuarioLogin);
+  }
+
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold ">
-        <form className="flex justify-center items-center flex-col w-1/2 gap-4" >
+        <form
+          className="flex justify-center items-center flex-col w-1/2 gap-4"
+          onSubmit={login}
+        >
           <h2 className="text-slate-900 text-5xl ">Entrar</h2>
           <div className="flex flex-col w-full">
             <label htmlFor="email">Email</label>
@@ -16,7 +49,10 @@ function Login() {
               name="email"
               placeholder="Email"
               className="border-2 border-slate-700 rounded p-2"
-        
+              value={usuarioLogin.email}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
             />
           </div>
           <div className="flex flex-col w-full">
@@ -27,19 +63,33 @@ function Login() {
               name="senha"
               placeholder="Senha"
               className="border-2 border-slate-700 rounded p-2"
-         
-              
+              value={usuarioLogin.senha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
             />
           </div>
-          <button  type='submit' className="rounded bg-indigo-400 hover:bg-indigo-900 text-white w-1/2 py-2 flex justify-center">
-          
-            <span>Entrar</span>
+          <button
+            type="submit"
+            className="rounded bg-indigo-400 hover:bg-indigo-900 text-white w-1/2 py-2 flex justify-center"
+          >
+            {isLoading ? (
+              <RotatingLines
+                strokeColor="white"
+                strokeWidth="5"
+                animationDuration="0.75"
+                width="24"
+                visible={true}
+              />
+            ) : (
+              <span>Entrar</span>
+            )}
           </button>
 
           <hr className="border-slate-800 w-full" />
 
           <p>
-            Ainda não tem uma conta?{' '}
+            Ainda não tem uma conta?{" "}
             <Link to="/cadastro" className="text-indigo-800 hover:underline">
               Cadastre-se
             </Link>
